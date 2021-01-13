c08fd58cc390b54cb3053417651da011341d38ac
Calum <34400717+bhop@users.noreply.github.com>
2021-01-13 00:10:37 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,15 @@
 import discord
 
+token = "your token" # put your token here
+clientID = "your client id" # put your client id here
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print("ready")
     async def on_message(self, message):
-        print("e")
-        if message.author.id == CLIENT ID:
+        print("e") # remove this if you want
+        if message.author.id == clientID:
             await message.edit(content=message.content + " ")
 
 client = MyClient()
-client.run("TOKEN HERE", bot=False)
+client.run(token, bot=False)
